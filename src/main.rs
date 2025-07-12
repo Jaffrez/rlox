3396eae23aaa5857b318308742c7d76371452573
Jaffrez <Jaffrez@outlook.com>
2025-07-12 22:58:28 +0800

    feat(rlox): setup project structure

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,19 @@
-fn main() {}
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() > 1 {
+        println!("Usage: rlox [script]");
+    } else if args.len() == 1 {
+        run_file(&args[1]);
+    } else {
+        run_prompt()
+    }
+}
+
+fn run_file(path: &str) {
+    unimplemented!()
+}
+
+fn run_prompt() {
+    unimplemented!()
+}
